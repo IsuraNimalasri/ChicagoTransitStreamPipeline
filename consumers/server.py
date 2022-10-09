c38021e6b20e4250ade39709bd308871968bf593
IsuraNimalasri <isuranimalasiri@gmail.com>
2022-10-09 14:00:29 +0100

    Update server.py

--- consumers/server.py
+++ consumers/server.py
@@ -63,18 +63,20 @@ def run_server():
     # Build kafka consumers
     consumers = [
         KafkaConsumer(
-            "org.chicago.cta.weather.v1",
+            # "org.chicago.cta.weather.v1",
+            "org.api.weather",
             weather_model.process_message,
             offset_earliest=True,
         ),
         KafkaConsumer(
-            "org.chicago.cta.stations.table.v1",
+            "org.chicago.cta.stations.transformed",
+            # "org.chicago.cta.stations.table.v1",
             lines.process_message,
             offset_earliest=True,
             is_avro=False,
         ),
         KafkaConsumer(
-            "^org.chicago.cta.station.arrivals.",
+            "^org.chicago.cta.*.arrivals",
             lines.process_message,
             offset_earliest=True,
         ),
